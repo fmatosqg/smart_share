4adac6b513b8c8715506a1036b9dfe4fa7a534b4
Fabio de Matos <fmatosqg@gmail.com>
2019-01-31 19:12:49 +1100

    Initial commit

---
+++ android/app/src/main/kotlin/net/amazingdomain/smartshare/MainActivity.kt
@@ -0,0 +1,42 @@
+package net.amazingdomain.smartshare
+
+import android.content.Intent
+import android.os.Bundle
+import io.flutter.app.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+
+class MainActivity : FlutterActivity() {
+
+    private var sharedText: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        GeneratedPluginRegistrant.registerWith(this)
+
+        val action = intent.action
+        val type = intent.type
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if ("text/plain".equals(type)) {
+                handleSendText(intent) // Handle text being sent
+            }
+        }
+
+        createMethodChannel()
+    }
+
+    fun handleSendText(intent: Intent) {
+        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+    }
+
+    fun createMethodChannel() {
+        MethodChannel(flutterView, "app.channel.shared.data").setMethodCallHandler { call, result ->
+            if (call.method.contentEquals("getSharedText")) {
+                result.success(sharedText)
+                sharedText = null
+            }
+        }
+    }
+}
---
+++ lib/view/ShareView.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SampleApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sample Shared App Handler',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: SampleAppPage(),
+    );
+  }
+}
+
+class SampleAppPage extends StatefulWidget {
+  SampleAppPage({Key key}) : super(key: key);
+
+  @override
+  _SampleAppPageState createState() => _SampleAppPageState();
+}
+
+class _SampleAppPageState extends State<SampleAppPage> {
+  static const platform = const MethodChannel('app.channel.shared.data');
+  String dataShared = "No data";
+
+  @override
+  void initState() {
+    super.initState();
+    getSharedText();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text(dataShared)));
+  }
+
+  getSharedText() async {
+    var sharedData = await platform.invokeMethod("getSharedText");
+    if (sharedData != null) {
+      setState(() {
+        dataShared = sharedData;
+      });
+    }
+  }
+}
